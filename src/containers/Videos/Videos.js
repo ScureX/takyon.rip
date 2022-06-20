f57631a98f5d940c5136cfd85d90a0ae1e487a7e
ScureX <multiaccshop@web.de>
2022-06-20 17:47:19 +0200

    new video

--- src/containers/Videos/Videos.js
+++ src/containers/Videos/Videos.js
@@ -6,8 +6,8 @@ const Videos = () => {
     <div className='Videos' id='contentBox'>
       <h1>Latest Videos</h1>
       <div className='VideoContent'>
-        <iframe src="https://www.youtube.com/embed/Lk9pPMT6UkU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe src="https://www.youtube.com/embed/J2mGl7bsO7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/VTa3wltqBUc" title="Breathe [4k] | Kraber 9k Clip Dump 3" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube.com/embed/J2mGl7bsO7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   )
